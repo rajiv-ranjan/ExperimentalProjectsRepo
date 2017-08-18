b2ea337e15279e17693db2271d5a618731fa19f2
Rajiv Ranjan <rajiv.smile@gmail.com>
2017-08-18 15:17:53 +0530

    Added Kafka Producer and Consumer Code

---
+++ FuseCamelKafkaIntegrationSpringDSL/src/main/java/com/rajiv/example/camel/kafka/springdsl/ProducerExchangeHeaders.java
@@ -0,0 +1,15 @@
+package com.rajiv.example.camel.kafka.springdsl;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.component.kafka.KafkaConstants;
+
+public class ProducerExchangeHeaders {
+
+	
+	public void loadKafkaProducerHeaders(Exchange exchange){
+		//System.out.println("exchange body class: "+exchange.getIn().getBody().getClass());
+		exchange.getIn().setBody("Test Message from Camel Kafka Component Final",String.class);
+        //exchange.getIn().setHeader(KafkaConstants.PARTITION_KEY, 0);
+        exchange.getIn().setHeader(KafkaConstants.KEY, "1");
+	}
+}
